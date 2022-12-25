16e85cf04776c5ea63d55b8b255f60bb67257ac8
Amir13862244 <120459185+Amir13862244@users.noreply.github.com>
2022-12-25 22:32:51 +0330

    Sxs.yftt19k.py
    Yftt15ksxs.photo.com

---
+++ Yftt18kfil-rubika.py
@@ -0,0 +1,37 @@
+while True:
+	print("gif.sxs.com")
+
+while True:
+	print("photo.sxs.com")
+while True:
+	print("pornhub.com")
+while True:
+	print("http://thepongduc.vn/vantindat/images/7159779569.html.sxs.com.#Sending_sexy_videos - Iran, Islamic Republic of #Sexy - https://www.pornhub.com - IP = [ 66.254.114.41 ] - Server = [ DNS2.P03.NSONE.NET > DNS3.P03.NSONE.NET > DNS4.P03.NSONE.NET > SDNS3.ULTRADNS.BIZ > SDNS3.ULTRADNS.COM > SDNS3.ULTRADNS.NET > SDNS3.ULTRADNS.ORG ] + https://www.rubika.ir - IP = [ 5.106.8.151 ] + Iran, Islamic Republic of - IP = 93.110.50.70 + [ #obscenity - #promoting violence - #Spam - #Phishing - #Sending_sexy_videos ]")
+
+
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
+while True:
+	print("https://github.com/HazratPantrexDGod/-Yftt_sex0018k-/blob/main/dxprit.Hazrat.Pantrex.hacker22.46d.god.filtering?raw=true")
